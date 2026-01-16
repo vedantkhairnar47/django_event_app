4df945a6a4357194849d886b64f19707a2c4306a
Vedant Khairnar <vedant3khairnar@gmail.com>
2026-01-16 15:57:57 +0530

    Added CSS

--- events/forms.py
+++ events/forms.py
@@ -7,28 +7,28 @@ class EventForm(forms.ModelForm):
         exclude = ['user']
         fields = ['name', 'date', 'time', 'description']
 
-        def clean(self):
-            cleaned_data = super().clean()
-            date = cleaned_data.get('date')
-            time = cleaned_data.get('time')
+    def clean(self):
+        cleaned_data = super().clean()
+        date = cleaned_data.get('date')
+        time = cleaned_data.get('time')
 
-            # user is passed from the view
-            user = self.instance.user if self.instance.pk else self.initial.get('user')
+        # user is passed from the view
+        user = self.instance.user if self.instance.pk else self.initial.get('user')
 
-            if date and time and user:
-                conflict = Event.objects.filter(
-                    user=user,
-                    date=date,
-                    time=time
+        if date and time and user:
+            conflict = Event.objects.filter(
+                user=user,
+                date=date,
+                time=time
+            )
+
+            # Exclude current event while updating
+            if self.instance.pk:
+                conflict = conflict.exclude(pk=self.instance.pk)
+
+            if conflict.exists():
+                raise forms.ValidationError(
+                    "You already have an event scheduled at this date and time."
                 )
 
-                # Exclude current event while updating
-                if self.instance.pk:
-                    conflict = conflict.exclude(pk=self.instance.pk)
-
-                if conflict.exists():
-                    raise forms.ValidationError(
-                        "You already have an event scheduled at this date and time."
-                    )
-
-            return cleaned_data
+        return cleaned_data
